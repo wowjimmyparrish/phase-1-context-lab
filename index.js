76fac0e971503a8881dfc1358dd442466b110afa
James Parrish <jthomasparrishjr@gmail.com>
2022-07-10 10:37:33 -0500

    commit tree e54bd8f3ad75f99c24bc5bebc11fb1c2268ce982

--- index.js
+++ index.js
@@ -1,4 +1,81 @@
 /* Your Code Here */
+const createEmployeeRecord = (employee) => {
+    return  {firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+   }
+}
+
+const createEmployeeRecords = (employees) => {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+// const createTimeInEvent = (event) => {
+//      let [date, hour] = event.split(" ")
+//      let eventObj = {
+//         type: "TimeIn",
+//         hour: parseInt(hour, 10),
+//         date
+//     }
+//     this.timeInEvents.push(eventObj)
+//     return this
+// }
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(soughtDate){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+  
+  let calculatePayroll = function(arrayOfEmployeeRecords){
+      return arrayOfEmployeeRecords.reduce(function(memo, rec){
+          return memo + allWagesFor.call(rec)
+      }, 0)
+  }
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
